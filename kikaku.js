1c35a66d92bf824306806a6a6fba2595d1bcabf2
hona09100520 <hannah.sakaguchi@gmail.com>
2025-06-24 08:28:01 +0900

    Update kikaku.js

---
+++ kikaku.js
@@ -0,0 +1,15 @@
+        function changeImage(index) {
+            // 画像の切り替え
+            let images = document.querySelectorAll(".ichiran img");
+            images.forEach((img, i) => {
+                img.classList.toggle("active", i === index);
+            });
+
+            // ボタンのスタイル変更
+            let buttons = document.querySelectorAll(".tab-button");
+            buttons.forEach((btn, i) => {
+                btn.classList.toggle("active", i === index);
+            });
+        }
+
+        
